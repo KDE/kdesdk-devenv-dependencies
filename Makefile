3d987d4a86c0b8b4e56bb5c9de211ecf4e5739cd
Aleix Pol <aleixpol@kde.org>
2016-11-09 17:00:11 +0100

    Make sure arch package has appstream installed

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ check:
 arch: archlinux/PKGBUILD.in ./archlinux/genpkgbuild.py
 	mkdir -p archlinux-output/
 	./archlinux/genpkgbuild.py
+	install org.kde.development.appdata.xml archlinux-output/
--- archlinux/genpkgbuild.py
+++ archlinux/genpkgbuild.py
@@ -2,14 +2,26 @@
 
 import sys
 import json
+import hashlib
 import subprocess
+
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def main():
     jsoninfo = subprocess.check_output(["./packages", "packages-json"])
     deps = json.loads(jsoninfo.decode('utf-8'))
 
     with open('archlinux/PKGBUILD.in') as infile, open('archlinux-output/PKGBUILD', 'w') as outfile:
-        replacements = { "@DEPENDS@": " ".join(deps["required"]), "@OPTDEPENDS@": " ".join(deps["suggested"]) }
+        replacements = {
+            "@DEPENDS@": " ".join(deps["required"]),
+            "@OPTDEPENDS@": " ".join(deps["suggested"]),
+            "@MD5@": md5("org.kde.development.appdata.xml")
+        }
 
         for line in infile:
             for src, target in replacements.items():
